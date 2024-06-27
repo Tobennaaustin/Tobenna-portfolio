32fb844d0f9e61567e3e780fa1e79ce243d4e98c
Teddy Austin Tobenna <136184722+Tobennaaustin@users.noreply.github.com>
2024-06-27 15:32:13 +0000

    Create nav.js

---
+++ JS/nav.js
@@ -0,0 +1,52 @@
+function openGithub() {
+    window.location.href = "https://github.com/Tobennaaustin/";
+}
+
+function openInsta() {
+    window.location.href = "https://instagram.com/teddyaustin19?igshid=ZGUzMzM3NWJiOQ==";
+}
+
+function openTwitter() {
+    window.location.href = "https://x.com/AustinTobenna?t=61GTnFwyJWpzSHY5fPGFhA&s=09";
+}
+
+function openWhatsapp() {
+    window.location.href = "https://wa.me/+2347045961380";
+}
+function openPort() {
+    window.location.href = "https://tobenna-portfolio.vercel.app/";
+}
+function openNote() {
+    window.location.href = "https://note-buddy.vercel.app/";
+}
+function openVote() {
+    window.location.href = "https://github.com/Tobennaaustin/Nacos-Election-site";
+}
+
+function openValidkey() {
+    window.location.href = "https://valid-passkey.vercel.app/?";
+}
+
+function openWeather() {
+    window.location.href = "https://weather-app-ruddy-two-56.vercel.app/";
+}
+
+function openToDO() {
+    window.location.href = "https://todo-app-eight-swart-55.vercel.app/";
+}
+
+function openDigitalStudent() {
+    window.location.href = "https://digital-student-care2.vercel.app/";
+}
+
+function openResourceHub() {
+    window.location.href = "https://resourcehub-three.vercel.app/";
+}
+
+function openQuizApp() {
+    window.location.href = "https://quizzo-com.vercel.app/";
+}
+
+function openMusicPlayer() {
+    window.location.href = "https://weather-app-ruddy-two-56.vercel.app/";
+}
